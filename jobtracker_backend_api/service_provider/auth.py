b791e460ffc0ae0565b3a26c61d3080e4ac8feab
Nga Vu <vuthiquynhngahvn@gmail.com>
2025-06-01 00:21:08 -0700

    Handle exceptions in CookieJWTAuthentication to prevent errors during token validation

--- jobtracker_backend_api/service_provider/auth.py
+++ jobtracker_backend_api/service_provider/auth.py
@@ -5,5 +5,8 @@ class CookieJWTAuthentication(JWTAuthentication):
         token = request.COOKIES.get('access_token')
         if not token:
             return None
-        validated_token = self.get_validated_token(token)
-        return self.get_user(validated_token), validated_token
+        try:
+            validated_token = self.get_validated_token(token)
+            return self.get_user(validated_token), validated_token
+        except Exception:
+            return None  # Do not raise, just return None
