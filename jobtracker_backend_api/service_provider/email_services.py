77dd6789ccd06493e146ac7b57dad0a92ecfd944
Nga Vu <vuthiquynhngahvn@gmail.com>
2025-06-12 18:44:02 -0700

    Refactor email_services.py to improve email fetching logic and enhance after date calculation; update .bandit configuration for consistency.

--- jobtracker_backend_api/service_provider/email_services.py
+++ jobtracker_backend_api/service_provider/email_services.py
@@ -74,11 +74,77 @@ def extract_body(mime_msg):
 def get_user_job_count(user):
     return JobApplied.objects.filter(user=user).count()
 
-def get_after_date(last_fetch_date, now):
-    if last_fetch_date.date() == now.date():
-        return now.replace(hour=0, minute=0, second=0, microsecond=0)
-    return last_fetch_date
 
+def get_after_date(user):
+    fetch_log = FetchLog.objects.filter(user=user).order_by("-last_fetch_date").first()
+    last_fetch_date = fetch_log.last_fetch_date if fetch_log else None
+    after_date = get_after_date(last_fetch_date, timezone.now())
+    return after_date
+
+
+def get_messages(gmail_service, after_date_string, next_page_token=None, batch_size=10):
+    """Fetch messages from Gmail after a specific date."""
+    try:
+        results = (
+            gmail_service.users()
+            .messages()
+            .list(
+                userId="me",
+                q=f"after:{after_date_string}",
+                maxResults=batch_size,
+                pageToken=next_page_token,
+            )
+            .execute()
+        )
+        return results.get("messages", []), results.get("nextPageToken")
+    except HttpError as error:
+        print(f"An error occurred while fetching messages: {error}")
+        return [], None
+
+
+def classify_email(gmail_service, user, msg, curr_job_count):
+    msg_data = (
+        gmail_service.users()
+        .messages()
+        .get(userId="me", id=msg["id"], format="raw")
+        .execute()
+    )
+    if "raw" not in msg_data:
+        print(f"Message {msg['id']} does not have raw content, skipping.")
+        return None, None, None, None
+
+    mime_msg = email.message_from_bytes(base64.urlsafe_b64decode(msg_data["raw"]))
+    sender = mime_msg["from"]
+    subject = mime_msg["subject"] if mime_msg["subject"] else "No Subject"
+
+    # Extract email body
+    body = extract_body(mime_msg)
+
+    # Extract job application data
+    (
+        is_job_application_email,
+        job_title,
+        company_name,
+        application_status,
+    ) = extract_email_data(subject, body)
+
+    if is_job_application_email:
+        job_applied, created = JobApplied.objects.get_or_create(
+            user=user,
+            job_title=job_title,
+            company=company_name,
+            defaults={
+                "status": application_status,
+                "sender_email": sender,
+                "row_number": curr_job_count + 1,
+            },
+        )
+        if created:
+            curr_job_count += 1
+        job_applied.status = application_status
+        job_applied.save()
+
+    return job_title, company_name, application_status, job_applied
 
 
 def get_emails(user):
@@ -97,24 +163,10 @@ def get_emails(user):
         print(f"User job count: {user_job_count}")
         curr_job_count = user_job_count + 1
 
-        fetch_log = (
-            FetchLog.objects.filter(user=user).order_by("-last_fetch_date").first()
-        )
-        if fetch_log and fetch_log.last_fetch_date:
-            last_fetch_date = fetch_log.last_fetch_date
-        else:
-            # return error if no fetch log exists
-            print("No fetch log found for the user.")
-            return
+        after_date = get_after_date(user)
 
-        now = datetime.now(timezone.utc)
-        if last_fetch_date.date() == now.date():
-            after_date = now.replace(hour=0, minute=0, second=0, microsecond=0)
-        else:
-            after_date = last_fetch_date
-
-        after_date = after_date.strftime("%Y/%m/%d")
-        print(f"Fetching emails after: {after_date}")
+        after_date_string = after_date.strftime("%Y/%m/%d")
+        print(f"Fetching emails after: {after_date_string}")
 
         # Pagination setup
         next_page_token = None
@@ -122,64 +174,18 @@ def get_emails(user):
         batch_size = int(os.getenv("FETCH_BATCH_SIZE", 10))  # Adjust as needed
 
         while True:
-            results = (
-                gmail_service.users()
-                .messages()
-                .list(
-                    userId="me",
-                    q=f"after:{after_date}",
-                    maxResults=batch_size,
-                    pageToken=next_page_token,
-                )
-                .execute()
+            messages, next_page_token = get_messages(
+                gmail_service, after_date_string, next_page_token, batch_size
             )
-            messages = results.get("messages", [])
             print(f"Fetched {len(messages)} messages in this batch.")
             total_fetched += len(messages)
 
             job_list = []
             for msg in messages:
-                msg_data = (
-                    gmail_service.users()
-                    .messages()
-                    .get(userId="me", id=msg["id"], format="raw")
-                    .execute()
+                job_title, company_name, application_status, job_applied = (
+                    classify_email(gmail_service, user, msg, curr_job_count)
                 )
-                if "raw" not in msg_data:
-                    print(f"Message {msg['id']} does not have raw content, skipping.")
-                    continue
-                mime_msg = email.message_from_bytes(
-                    base64.urlsafe_b64decode(msg_data["raw"])
-                )
-                sender = mime_msg["from"]
-                subject = mime_msg["subject"] if mime_msg["subject"] else "No Subject"
-
-                # Extract email body
-                body = extract_body(mime_msg)
-
-                # Extract job application data
-                (
-                    is_job_application_email,
-                    job_title,
-                    company_name,
-                    application_status,
-                ) = extract_email_data(subject, body)
-
-                if is_job_application_email:
-                    job_applied, created = JobApplied.objects.get_or_create(
-                        user=user,
-                        job_title=job_title,
-                        company=company_name,
-                        defaults={
-                            "status": application_status,
-                            "sender_email": sender,
-                            "row_number": curr_job_count + 1,
-                        },
-                    )
-                    if created:
-                        curr_job_count += 1
-                    job_applied.status = application_status
-                    job_applied.save()
+                if job_title:
                     job_list.append(
                         {
                             "job_title": job_title,
@@ -197,12 +203,11 @@ def get_emails(user):
                 print(f"Added {len(job_list)} jobs to the Google Sheet.")
 
             # Check for next page
-            next_page_token = results.get("nextPageToken")
             if not next_page_token:
                 break  # No more pages
 
         # Create fetch log with the current date
-        FetchLog.objects.create(last_fetch_date=now, user=user)
+        FetchLog.objects.create(last_fetch_date=datetime.now(timezone.utc), user=user)
         print(f"Total emails fetched: {total_fetched}")
 
     except HttpError as error:
