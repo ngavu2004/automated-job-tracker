11b617afd4aa356d3f77c5e95b1628ee595e4815
Nga Vu <vuthiquynhngahvn@gmail.com>
2025-07-05 15:16:23 -0700

    set gg authenticate function as internal

--- jobtracker_backend_api/service_provider/authenticate.py
+++ jobtracker_backend_api/service_provider/authenticate.py
@@ -4,7 +4,7 @@ from google.oauth2.credentials import Credentials
 from googleapiclient.discovery import build
 
 
-def get_google_auth_credentials(google_access_token, google_refresh_token):
+def _get_google_auth_credentials(google_access_token, google_refresh_token):
     """Get Google OAuth credentials for the user."""
     creds = Credentials(
         token=google_access_token,
@@ -18,7 +18,7 @@ def get_google_auth_credentials(google_access_token, google_refresh_token):
 
 def get_gmail_service(google_access_token, google_refresh_token):
     """Authenticate and return Gmail service clients."""
-    creds = get_google_auth_credentials(google_access_token, google_refresh_token)
+    creds = _get_google_auth_credentials(google_access_token, google_refresh_token)
     try:
         gmail_service = build("gmail", "v1", credentials=creds)
         return gmail_service
@@ -29,6 +29,6 @@ def get_gmail_service(google_access_token, google_refresh_token):
 
 def get_googlesheet_service(google_access_token, google_refresh_token):
     """Authenticate and return Google Sheets service client."""
-    creds = get_google_auth_credentials(google_access_token, google_refresh_token)
+    creds = _get_google_auth_credentials(google_access_token, google_refresh_token)
     sheets_service = build("sheets", "v4", credentials=creds)
     return sheets_service
